e28f19fd5e90cd7c11042b0dafb74c88649db273
brubcov <brubcov@mmtr.ru>
2023-04-10 16:38:22 +0300

    rework reports part 1

--- src/main/java/com/example/cargo_transportation/repo/JournalRepository.java
+++ src/main/java/com/example/cargo_transportation/repo/JournalRepository.java
@@ -12,18 +12,19 @@ import java.util.List;
 public interface JournalRepository extends JpaRepository<Journal, Long> {
     List<Journal> findJournalsByOutFactDateNullAndCar_Id(Long carId);
 
-    @Query(value = "SELECT j.waybill as waybill, \n" +
-                    "       j.incoming_date as incomingDateTime, \n" +
-                    "       c.gos_num as gosNum, \n" +
-                    "       s.name as service \n" +
-                    "FROM journal as j \n" +
-                    "JOIN car as c on j.car_id = c.id \n" +
-                    "JOIN client as cl on c.client_id = cl.id \n" +
-                    "LEFT JOIN get_service as gs on j.id = gs.journal_id \n" +
-                    "LEFT JOIN service as s on gs.service_id = s.id \n" +
-                    "WHERE c.gos_num = COALESCE(:gosNum, c.gos_num) and \n" +
-                    "cl.id = COALESCE(:clientId, cl.id) and \n" +
-                    "j.incoming_date between :startDate and :endDate",
+    @Query( value =
+            "SELECT j.waybill as waybill, \n" +
+            "       j.incoming_date as incomingDateTime, \n" +
+            "       c.gos_num as gosNum, \n" +
+            "       s.name as service \n" +
+            "FROM journal as j \n" +
+            "JOIN car as c on j.car_id = c.id \n" +
+            "JOIN client as cl on c.client_id = cl.id \n" +
+            "LEFT JOIN get_service as gs on j.id = gs.journal_id \n" +
+            "LEFT JOIN service as s on gs.service_id = s.id \n" +
+            "WHERE c.gos_num = COALESCE(:gosNum, c.gos_num) and \n" +
+            "cl.id = COALESCE(:clientId, cl.id) and \n" +
+            "j.incoming_date between :startDate and :endDate",
             nativeQuery = true)
     List<JournalReport> getJournalReport(@Param("gosNum") String gosNum,
                                          @Param("clientId") Long clientId,
