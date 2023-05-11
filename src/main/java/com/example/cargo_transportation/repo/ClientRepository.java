d4ede797379276723a3762c63e851c1a604fd041
Bogdan <datevbmy@gmail.com>
2023-05-12 00:47:47 +0300

    end rework 2

--- src/main/java/com/example/cargo_transportation/repo/ClientRepository.java
+++ src/main/java/com/example/cargo_transportation/repo/ClientRepository.java
@@ -1,7 +1,6 @@
 package com.example.cargo_transportation.repo;
 
 import com.example.cargo_transportation.entity.Client;
-import com.example.cargo_transportation.modal.report.JournalReport;
 import com.example.cargo_transportation.modal.report.StatisticsReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,18 +8,17 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
     @Query(value = "SELECT csc.clientId as clientId, sum(csc.count) as count, sum(csc.count * p.cost) as cost, " +
             "min(csc.incomingDate) as incomingDateMin, max(csc.incomingDate) as incomingDateMax \n" +
             "FROM (SELECT cl.id as clientId, c.Id as carId, j.incoming_date as incomingDate,\n" +
-            "             s.id as serviceId, gs.count as count \n" +
+            "             s.id as serviceId, ps.count as count \n" +
             "        FROM client cl \n" +
             "        INNER JOIN car c on cl.id = c.client_id \n" +
             "        INNER JOIN journal j on c.id = j.car_id \n" +
-            "        INNER JOIN get_service gs on j.id = gs.journal_id \n" +
-            "        INNER JOIN service s on gs.service_id = s.id \n" +
+            "        INNER JOIN provide_service ps on j.id = ps.journal_id \n" +
+            "        INNER JOIN service s on ps.service_id = s.id \n" +
             "        WHERE cl.Id = COALESCE(:clientId, cl.id) and \n" +
             "              j.incoming_date between :startDate and :endDate \n" +
             "    ) as csc\n" +
